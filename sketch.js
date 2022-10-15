8ca0c15005b0cff3d526e72f38d0f0501c931832
rakel <rakelspahi03@gmail.com>
2022-10-15 11:19:39 +0200

    Fixed discrepancy regarding cells[] array. Maze layout now generates successfully.

--- sketch.js
+++ sketch.js
@@ -1,53 +1,78 @@
 var cols, rows;
-var w = 40;
+var w = 20;
 var grid = [];
-function setup() {
-    createCanvas(400, 400);
-    cols = floor(width/w);
-    rows = floor(height/w);
 
-    for (var i = 0; i < rows; j++) {
-        for (var i = 0; i < cols; i++) {
-            var cell = new Cell(i,j);
-            grid.push(cell);
-        }
+var current;
+
+var stack = [];
+
+function setup() {
+  createCanvas(600, 600);
+  cols = floor(width / w);
+  rows = floor(height / w);
+
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      var cell = new Cell(i, j);
+      grid.push(cell);
     }
+  }
+
+  current = grid[0];
+
 
 }
 
 function draw() {
-    background(220);
-    for (var i=0; i< cells.length; i++) {
-        cells[i].show();
-    }
+  background(51);
+  for (var i = 0; i < grid.length; i++) {
+    grid[i].show();
+  }
+
+  current.visited = true;
+  current.highlight();
+  // STEP 1
+  var next = current.checkNeighbors();
+  if (next) {
+    next.visited = true;
+
+    // STEP 2
+    stack.push(current);
+
+    // STEP 3
+    removeWalls(current, next);
+
+    // STEP 4
+    current = next;
+  } else if (stack.length > 0) {
+    current = stack.pop();
+  }
 
 }
 
-function Cell(i, j) {
-    this.i = i;
-    this.j = j;
-    this.walls = [true, true, true, true];
+function index(i, j) {
+  if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+    return -1;
+  }
+  return i + j * cols;
+}
 
-this.show = function() {
-    var x = this.i*w;
-    var y = this.j*w;
-    stroke(255);
-    if (this.walls[0]) {
-        line(x    , y   , x + w, y);
-    }
-    if (this.walls[1]) {
-    line(x + w, y   , x + w, y + w);
-    }
-    if (this.walls[2]) {
-    line(x + w, y + w,x    , y + w);
-    }
-    if (this.walls[3]) {
-    line(x    , y + w,x    , y);
-    }
 
-    //noFIll();
-    //rect(x,y,w,w);
-
-    }
-
+function removeWalls(a, b) {
+  var x = a.i - b.i;
+  if (x === 1) {
+    a.walls[3] = false;
+    b.walls[1] = false;
+  } else if (x === -1) {
+    a.walls[1] = false;
+    b.walls[3] = false;
+  }
+  var y = a.j - b.j;
+  if (y === 1) {
+    a.walls[0] = false;
+    b.walls[2] = false;
+  } else if (y === -1) {
+    a.walls[2] = false;
+    b.walls[0] = false;
+  }
 }
